afccb5e386044b91d24595599349d92eaf95138a
kwy8791 <kwy8791+gh@gmail.com>
2018-04-16 10:34:21 +0900

    until page 83

---
+++ code/p83_containers_in_a_container.py
@@ -0,0 +1,11 @@
+lists = []
+
+rap = ["カニエ・ウェスト", "ジェイ・Z", "エミネム", "ナズ"]
+rock = ["ボブ・ディラン", "ザ・ビートルズ", "レッド・ツェッペリン"]
+djs = ["ゼッズ・デッド", "ティエスト"]
+
+lists.append(rap)
+lists.append(rock)
+lists.append(djs)
+
+print(lists)
